13ddae455fce7c1342d291ecf2795b7f46141259
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-08-03 17:24:15 +0900

    一時保存

---
+++ module-08/module08_exercise.ts
@@ -0,0 +1,16 @@
+namespace Greetings {
+  export function returnGreeting(greeting: string) {
+    console.log(`The message from namespace Greetings is ${greeting}.`);
+  }
+}
+namespace GreetingsWithLength {
+  export function returnGreeting(greeting: string) {
+    let greetingLength = getLength(greeting);
+    console.log(
+      `The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`
+    );
+  }
+  function getLength(message: string): number {
+    return message.length;
+  }
+}
